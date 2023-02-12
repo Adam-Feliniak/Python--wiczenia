66a3bcd7bd2b261162ad179e60267aa9f84ec660
Adam-Feliniak <adddzik@gmail.com>
2023-02-12 22:09:15 +0100

    make folder structure and fullfil chapter 'zmienne i typy danych'

--- Instrukcje dla programisty/Zmienne i typy danych/apostrophe.py
+++ Instrukcje dla programisty/Zmienne i typy danych/apostrophe.py
@@ -1,2 +1,3 @@
+#Każda podróż, zaczyna się od pierwszego kroku
 message = "Dla programisty Johna O'Hary jedną z zalet Pythona jest jego wszechstronność i oddana mu społeczność."
 print(message)
--- Instrukcje dla programisty/Zmienne i typy danych/slawny_cytat_2.py
+++ Instrukcje dla programisty/Zmienne i typy danych/slawny_cytat_2.py
@@ -1,3 +1,4 @@
+#Mój ulubiony cytat
 quote = "Sukces polega na przechodzeniu od porażki do porażki bez utraty entuzjazmu."
 famous_person = "Winston Churchill"
 message = f"{famous_person} powiedział kiedyś: \x1B[3m\"{quote}\"\x1B[0m"
