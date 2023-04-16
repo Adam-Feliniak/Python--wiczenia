600702cc911f17056edac4973638c50f95b3acb7
Adam-Feliniak <adddzik@gmail.com>
2023-04-17 01:18:06 +0200

    - fields_and_methods2.py

---
+++ OOP/fields_and_methods2.py
@@ -0,0 +1,45 @@
+class Car:
+    how_many_cars_so_far = 0
+    def __init__(self, brand, model, color):
+        self.brand = brand
+        self.model = model
+        self.color = color
+        self.velocity = 0
+        Car.how_many_cars_so_far += 1
+
+    def speed_up(self):
+        self.velocity += 1
+
+    def slow_down(self):
+        self.velocity -= 1
+        if self.velocity < 0:
+            self.velocity = 0
+    @classmethod
+    def get_name(cls):
+        if cls.how_many_cars_so_far == 0:
+            return "samochodów"
+        elif cls.how_many_cars_so_far == 1:
+            return "samochód"
+        elif cls.how_many_cars_so_far < 5:
+            return "samochody"
+        else:
+            return "samochodów"
+
+print(Car.how_many_cars_so_far)
+
+my_car = Car("Daewoo", "Lanos", "zielony")
+print(Car.how_many_cars_so_far)
+my_car.speed_up()
+print(my_car.velocity)
+my_car.slow_down()
+my_car.slow_down()
+my_car.slow_down()
+print(my_car.velocity)
+renault = Car('Renault', 'Ancara', 'czerwony')
+fiat = Car('Fiat', '126p', 'silver')
+fiat2 = Car('Fiat', '126p', 'silver')
+fiat3 = Car('Fiat', '126p', 'silver')
+
+
+
+print(f"Jak na razie wyprodukowano {Car.how_many_cars_so_far} {Car.get_name()}.")
