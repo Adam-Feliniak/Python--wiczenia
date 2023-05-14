ab558a1f973aa06c3c9494ae034a91be69ac7237
Adam-Feliniak <adddzik@gmail.com>
2023-05-14 23:32:46 +0200

    - Lists

---
+++ Instrukcje dla programisty/Listy/cars.py
@@ -0,0 +1,20 @@
+cars = ["bmw", "audi", "toyota", "subaru"]
+cars.sort(reverse=True)
+print(cars)
+
+cars2 = ["bmw", "audi", "toyota", "subaru"]
+
+print("Oto lista początkowa:")
+print(cars2)
+
+print("\nOto lista posortowana:")
+print(sorted(cars2))
+
+print("\nOto lista początkowa:")
+print(cars2)
+
+cars3 = ["bmw", "audi", "toyota", "subaru"]
+print(cars3)
+cars3.reverse()
+print(cars3)
+print(len(cars3))
