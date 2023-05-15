e830caa455f189fbd909ebb5fae262fe7ff3120e
Adam-Feliniak <adddzik@gmail.com>
2023-05-15 21:57:22 +0200

    - magicians.py

---
+++ Instrukcje dla programisty/Praca z listą/magicans.py
@@ -0,0 +1,7 @@
+magicans = ['alicja', 'dawid', 'karolina']
+for magician in magicans:
+    print(f"{magician.title()}, to była doskonała sztuczka!")
+    print(f"Nie mogę doczekać się Twojej kolejnej sztuczki, {magician.title()}.\n")
+
+print("Dziękuję wszystkim. To był naprawdę wspaniały występ!")
+
